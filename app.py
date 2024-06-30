07f6c8f6bdc3bc55a0cd152610bbfda69759c582
Chaitany Chaudhary <85825826+ChaitanyChaudhary@users.noreply.github.com>
2024-06-30 16:41:37 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
-# import csv
-# import os
+
 from timezone_query import get_time_date
 
 
@@ -19,7 +18,7 @@ def get_Weather_data():
     user_location = request.form.get('locationInput')  # Get location from form data
     try:
         # api key is from https://www.weatherapi.com/
-        url = f"https://api.weatherapi.com/v1/current.json?key=b6851fd500f643ab9b3122134242506&q={user_location}"
+        url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={user_location}"
 
         # api request
         r = requests.get(url)
